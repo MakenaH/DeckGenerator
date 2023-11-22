a2866f27662b21e59cd7411e351c5ae09bc7fd8e
MakenaH <98350954+MakenaH@users.noreply.github.com>
2023-11-22 13:41:40 -0500

    Add project files.

---
+++ DeckGenerator/Card.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeckGenerator
+{
+    internal class Card
+    {
+        private string _name;
+        private string _description;
+        public bool _drawn;
+        public bool _continue;
+
+        public Card(bool continue_,string name_, string description_)
+        {
+            _name = name_;
+            _description = description_;
+            _drawn = false;
+            _continue = continue_;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+    }
+}
